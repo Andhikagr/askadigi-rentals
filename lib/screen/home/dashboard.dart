cd0b9bb7a3bd5c170c83f34a70797188fb66e20f
Andhika Gilang Rahadian <rahadian.kidz92@gmail.com>
2025-08-06 10:08:18 +0700

    Update all changes: UI, assets, packages, and other files

--- lib/screen/home/dashboard.dart
+++ lib/screen/home/dashboard.dart
@@ -38,6 +38,7 @@ class _DashboardState extends State<Dashboard> {
   void initState() {
     super.initState();
     _cars = loadCarsFromJson();
+    selectedBrand = "toyota";
   }
 
   String? selectedBrand;
