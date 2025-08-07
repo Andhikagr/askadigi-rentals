4b3b350a909d8e0aba0faf68b7019b48c3451aae
Andhika Gilang Rahadian <rahadian.kidz92@gmail.com>
2025-08-07 12:52:06 +0700

    push full project files

--- lib/core/utils/mainpage.dart
+++ lib/core/utils/mainpage.dart
@@ -1,3 +1,5 @@
+import 'package:car_rental/core/constant/colors.dart';
+import 'package:car_rental/core/utils/media_query.dart';
 import 'package:car_rental/screen/home/account.dart';
 import 'package:car_rental/screen/home/dashboard.dart';
 import 'package:car_rental/screen/home/order.dart';
@@ -17,16 +19,57 @@ class Mainpage extends StatelessWidget {
           index: controller.selectedIndex.value,
           children: controller.pages,
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: controller.selectedIndex.value,
-          onTap: (index) {
-            controller.selectedIndex.value = index;
-          },
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Orders'),
-          ],
+        bottomNavigationBar: Container(
+          height: context.shortp(0.18),
+          decoration: BoxDecoration(
+            color: surfaceColor(context),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade300,
+                blurRadius: 1,
+                offset: Offset(0, -1),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(controller.pages.length, (index) {
+              final isSelected = controller.selectedIndex.value == index;
+              final icon = controller.icons[index];
+              final label = controller.labels[index];
+
+              return Material(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(10),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(10),
+                  splashColor: Colors.grey.shade300,
+                  onTap: () => controller.selectedIndex.value = index,
+                  child: Container(
+                    width: context.shortp(0.3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: isSelected
+                          ? Colors.transparent
+                          : Colors.transparent,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          icon,
+                          color: isSelected
+                              ? outlineColor(context)
+                              : outlineColor(context),
+                        ),
+                        Text(label),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
         ),
       ),
     );
@@ -36,4 +79,6 @@ class Mainpage extends StatelessWidget {
 class Navcontroller extends GetxController {
   RxInt selectedIndex = 1.obs;
   final pages = [Account(), Dashboard(), Order()];
+  final icons = [Icons.person, Icons.home, Icons.receipt_long];
+  final labels = ["Account", "Home", "MyOrder"];
 }
